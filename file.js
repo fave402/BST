9871a1791dbfe320955f076745430f61893eacfa
unknown <favourw198@gmail.com>
2025-08-20 09:29:41 +0100

    initial commit

---
+++ file.js
@@ -0,0 +1,19 @@
+// Testimonial Slider
+ 
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  }
+});
